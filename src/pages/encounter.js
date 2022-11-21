22f7d7ffac010d9aeb305b3e5f28fc0ae8e24d36
Jarom Oyler <bubba@Bubba.local>
2022-11-21 08:53:59 -0700

    Updating

--- src/pages/encounter.js
+++ src/pages/encounter.js
@@ -3,24 +3,17 @@ import Collapse from 'react-bootstrap/Collapse';
 import React, { useState } from 'react';
 import RandomMonster1 from '../randomMaster'; 
 import {randomMonsterInfo, randomLandMark, randomTreasure, randomVerb,randomNPCinfo, numberOfMonsters, NumberOfNPC, randomEffect} from "../randomMaster"
-import Reuslt from "./result"
-
+import RandomResult from "./result"
+import useRandom from './useRandom';
 
 export default function Encounter() {
 
-    const [open, setOpen] = useState(false);
+    
     
     const [level, setLevel ] = useState(1-5)
 
-    const [verb, setVerb] = useState()
-    const [landmark, setLandMark] =useState()
-    const [treasure, setTreasure] =useState()
-    const [npc, setNPC] =useState()
-    const [monster, setMonster] =useState()
-    const [numberMonster, setNumberMonster] =useState()
-    const [numberNPC, setNumberNPC] =useState()
-    const [randomLandMarkEffect, setRandomEffect] =useState()
-
+    
+    const {verb,setVerb, landmark,setLandMark,treasure,setTreasure,npc,setNPC, monster,setMonster,numberMonster,setNumberMonster,numberNPC,setNumberNPC,randomLandMarkEffect,setRandomEffect} = useRandom()
 
     const clickHandle = event => {
         event.preventDefault()
@@ -41,7 +34,7 @@ export default function Encounter() {
         <NavComp></NavComp>
         <div>
             <form>
-            <label className="text-light"> Select Difficulty: 
+            <label className="text-light"> Select Difficulty:  
                 <select name="level" onChange={(e)=> setLevel(e.target.value)}>
                     <option value= {null} >Select Level </option>
                     <option value="1-5">Levels 1-5</option>
@@ -52,30 +45,13 @@ export default function Encounter() {
             <br />
             <input className="landing-button" type="submit" value="Generate Encounter" onClick={clickHandle} />
             </form>
-            <div className="npc-stats">
-                <span
-                    onClick={() => setOpen(!open)}
-                    aria-controls="example-collapse-text"
-                    aria-expanded={open}
-                ><i className="bi bi-chevron-down"></i></span>
-                <div className='hodler'>
-                    <h1>Monster Name</h1>
-                    <Collapse in={open}>
-                    
-                        <div id="example-collapse-text" className="text-light">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                        labore wes anderson cred nesciunt sapiente ea proident.
-                        </div>
-                    </Collapse>
-                </div>
-            </div>
-        </div>
-        <div className='text-light'>
-            <p>They are {numberMonster} {monster} at {landmark} The {monster} is {verb}</p> 
 
-            <p>{treasure}</p>
+
+            <RandomResult verb = {verb} landmark={landmark} treasure={treasure} npc={npc} monster={monster} numberMonster={numberMonster} numberNPC={numberNPC} randomLandMarkEffect={randomLandMarkEffect}></RandomResult>
+
+            
         </div>
+        
     </>
     )
   }
