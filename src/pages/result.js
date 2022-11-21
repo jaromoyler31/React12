917e97d88f3213bc1ca00eb56cb9c4e4e0d9d739
Jarom Oyler <bubba@Bubba.local>
2022-11-21 09:39:02 -0700

    Updating

--- src/pages/result.js
+++ src/pages/result.js
@@ -131,10 +131,10 @@ export default function RandomResult({verbs, landmark,treasure,npc, monster,numb
                 <p>They are {numberNPC} {npc} and {numberMonster} {monster}  at {landmark} The {monster} is {verbs} and the {npc} is {new_verb}</p> 
 
                 <p>Landmark Effect: {landEffect}</p>
-                <p>{treasure}</p>
+                <p>Treasure: {treasure}</p>
             </div>
             </>
 
         )
     }
-}
+} 
