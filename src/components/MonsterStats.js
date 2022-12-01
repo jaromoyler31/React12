1a9c9d04b25c145143a71f6ef42d5b5b14a48da0
Jarom Oyler <bubba@bubba.lan>
2022-12-01 13:22:18 -1000

    updating

--- src/components/MonsterStats.js
+++ src/components/MonsterStats.js
@@ -1,4 +1,4 @@
-import { monsterAC, monsterHP, monsterSpeed, monsterStr, monsterDex, monsterCon, monsterInt, monsterWis, monsterCha, monsterLang } from '../randomMaster';
+import { monsterAC, monsterHP, monsterSpeed, monsterStr, monsterDex, monsterCon, monsterInt, monsterWis, monsterCha, monsterLang,monsterSkills,monsterActions } from '../randomMaster';
 import Table from 'react-bootstrap/Table';
 
 
@@ -34,9 +34,197 @@ export default function MonsterStats() {
                 <li><span>Hit Points: </span>{monsterHP}</li>
                 <li><span>Speed: </span>{monsterSpeed}ft</li>
                 <li><span>Languages: </span>{monsterLang}</li>
+                <hr></hr>
+                {skills()}
+                <hr></hr>
+                {actions()}
             </ul>
         </div>
     </div>
 
   );
+}
+ 
+
+function skills(){
+    
+    if(monsterSkills.length===1){
+        return(
+            <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+        )
+     }
+    if(monsterSkills.length===2){
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+            </>
+            
+        )
+     }
+    if(monsterSkills.length===3){
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+                <li><span>{monsterSkills[2].name}: </span>{monsterSkills[2].desc}</li>
+
+            </>
+            
+        )
+    }
+    if(monsterSkills.length===4){
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+                <li><span>{monsterSkills[2].name}: </span>{monsterSkills[2].desc}</li>
+                <li><span>{monsterSkills[3].name}: </span>{monsterSkills[3].desc}</li>
+            </>
+            
+        )
+     }
+    if(monsterSkills.length===5){
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+                <li><span>{monsterSkills[2].name}: </span>{monsterSkills[2].desc}</li>
+                <li><span>{monsterSkills[3].name}: </span>{monsterSkills[3].desc}</li>
+                <li><span>{monsterSkills[4].name}: </span>{monsterSkills[4].desc}</li>
+
+            </>
+            
+        )
+    }if(monsterSkills.length===6){
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+                <li><span>{monsterSkills[2].name}: </span>{monsterSkills[2].desc}</li>
+                <li><span>{monsterSkills[3].name}: </span>{monsterSkills[3].desc}</li>
+                <li><span>{monsterSkills[4].name}: </span>{monsterSkills[4].desc}</li>
+                <li><span>{monsterSkills[5].name}: </span>{monsterSkills[5].desc}</li>
+
+
+            </>
+            
+        )
+    }if(monsterSkills.length===7){
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+                <li><span>{monsterSkills[2].name}: </span>{monsterSkills[2].desc}</li>
+                <li><span>{monsterSkills[3].name}: </span>{monsterSkills[3].desc}</li>
+                <li><span>{monsterSkills[4].name}: </span>{monsterSkills[4].desc}</li>
+                <li><span>{monsterSkills[5].name}: </span>{monsterSkills[5].desc}</li>
+                <li><span>{monsterSkills[6].name}: </span>{monsterSkills[6].desc}</li>
+
+
+
+            </>
+            
+        )
+    }
+    if(monsterSkills.length===0){
+        return(
+            <h2>skills:none</h2>
+        )        
+    }else{
+        return(
+            <>
+                <li><span>{monsterSkills[0].name}: </span>{monsterSkills[0].desc}</li>
+                <li><span>{monsterSkills[1].name}: </span>{monsterSkills[1].desc}</li>
+                <li><span>{monsterSkills[2].name}: </span>{monsterSkills[2].desc}</li>
+                <li><span>{monsterSkills[3].name}: </span>{monsterSkills[3].desc}</li>
+                <li><span>{monsterSkills[4].name}: </span>{monsterSkills[4].desc}</li>
+                <li><span>{monsterSkills[5].name}: </span>{monsterSkills[5].desc}</li>
+            </>
+            
+        )
+    }
+    
+    
+
+
+
+    
+}
+
+function actions(){
+    if(monsterActions.length === 1){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+            </>
+        )
+    }if(monsterActions.length === 2){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+                <li><span>{monsterActions[1].name}: </span>{monsterActions[1].desc}</li>
+
+            </>
+        )
+    }if(monsterActions.length === 3){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+                <li><span>{monsterActions[1].name}: </span>{monsterActions[1].desc}</li>
+                <li><span>{monsterActions[2].name}: </span>{monsterActions[2].desc}</li>
+
+
+            </>
+        )
+    }if(monsterActions.length === 4){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+                <li><span>{monsterActions[1].name}: </span>{monsterActions[1].desc}</li>
+                <li><span>{monsterActions[2].name}: </span>{monsterActions[2].desc}</li>
+                <li><span>{monsterActions[3].name}: </span>{monsterActions[3].desc}</li>
+
+
+
+            </>
+        )
+    }if(monsterActions.length === 5){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+                <li><span>{monsterActions[1].name}: </span>{monsterActions[1].desc}</li>
+                <li><span>{monsterActions[2].name}: </span>{monsterActions[2].desc}</li>
+                <li><span>{monsterActions[3].name}: </span>{monsterActions[3].desc}</li>
+                <li><span>{monsterActions[4].name}: </span>{monsterActions[4].desc}</li>
+            </>
+        )
+    }if(monsterActions.length === 6){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+                <li><span>{monsterActions[1].name}: </span>{monsterActions[1].desc}</li>
+                <li><span>{monsterActions[2].name}: </span>{monsterActions[2].desc}</li>
+                <li><span>{monsterActions[3].name}: </span>{monsterActions[3].desc}</li>
+                <li><span>{monsterActions[4].name}: </span>{monsterActions[4].desc}</li>
+                <li><span>{monsterActions[5].name}: </span>{monsterActions[5].desc}</li>
+
+            </>
+        )
+    }if(monsterActions.length === 7){
+        return(
+            <>
+                <li><span>{monsterActions[0].name}: </span>{monsterActions[0].desc}</li>
+                <li><span>{monsterActions[1].name}: </span>{monsterActions[1].desc}</li>
+                <li><span>{monsterActions[2].name}: </span>{monsterActions[2].desc}</li>
+                <li><span>{monsterActions[3].name}: </span>{monsterActions[3].desc}</li>
+                <li><span>{monsterActions[4].name}: </span>{monsterActions[4].desc}</li>
+                <li><span>{monsterActions[5].name}: </span>{monsterActions[5].desc}</li>
+                <li><span>{monsterActions[6].name}: </span>{monsterActions[6].desc}</li>
+            </>
+        )
+    }
+    
+    
+    
 }
--- src/components/NpcStats.js
+++ src/components/NpcStats.js
@@ -1,4 +1,4 @@
-import { npcAC, npcHP, npcSpeed, npcStr, npcDex, npcCon, npcInt, npcWis, npcCha, npcLang } from '../randomMaster';
+import { npcAC, npcHP, npcSpeed, npcStr, npcDex, npcCon, npcInt, npcWis, npcCha, npcLang, npcActions, npcSkills } from '../randomMaster';
 import Table from 'react-bootstrap/Table';
 
 
@@ -48,6 +48,194 @@ export default function MonsterStats() {
         </div>
     </div>
     </div>
+    <hr></hr>
+    {skills()}
+    <hr></hr>
+    {actions()}
     </>
   );
+}
+
+
+function skills(){
+    
+    if(npcSkills.length===1){
+        return(
+            <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+        )
+     }
+    if(npcSkills.length===2){
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+            </>
+            
+        )
+     }
+    if(npcSkills.length===3){
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+                <li><span>{npcSkills[2].name}: </span>{npcSkills[2].desc}</li>
+
+            </>
+            
+        )
+    }
+    if(npcSkills.length===4){
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+                <li><span>{npcSkills[2].name}: </span>{npcSkills[2].desc}</li>
+                <li><span>{npcSkills[3].name}: </span>{npcSkills[3].desc}</li>
+            </>
+            
+        )
+     }
+    if(npcSkills.length===5){
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+                <li><span>{npcSkills[2].name}: </span>{npcSkills[2].desc}</li>
+                <li><span>{npcSkills[3].name}: </span>{npcSkills[3].desc}</li>
+                <li><span>{npcSkills[4].name}: </span>{npcSkills[4].desc}</li>
+
+            </>
+            
+        )
+    }if(npcSkills.length===6){
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+                <li><span>{npcSkills[2].name}: </span>{npcSkills[2].desc}</li>
+                <li><span>{npcSkills[3].name}: </span>{npcSkills[3].desc}</li>
+                <li><span>{npcSkills[4].name}: </span>{npcSkills[4].desc}</li>
+                <li><span>{npcSkills[5].name}: </span>{npcSkills[5].desc}</li>
+
+
+            </>
+            
+        )
+    }if(npcSkills.length===7){
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+                <li><span>{npcSkills[2].name}: </span>{npcSkills[2].desc}</li>
+                <li><span>{npcSkills[3].name}: </span>{npcSkills[3].desc}</li>
+                <li><span>{npcSkills[4].name}: </span>{npcSkills[4].desc}</li>
+                <li><span>{npcSkills[5].name}: </span>{npcSkills[5].desc}</li>
+                <li><span>{npcSkills[6].name}: </span>{npcSkills[6].desc}</li>
+
+
+
+            </>
+            
+        )
+    }
+    if(npcSkills.length===0){
+        return(
+            <h2>skills:none</h2>
+        )        
+    }else{
+        return(
+            <>
+                <li><span>{npcSkills[0].name}: </span>{npcSkills[0].desc}</li>
+                <li><span>{npcSkills[1].name}: </span>{npcSkills[1].desc}</li>
+                <li><span>{npcSkills[2].name}: </span>{npcSkills[2].desc}</li>
+                <li><span>{npcSkills[3].name}: </span>{npcSkills[3].desc}</li>
+                <li><span>{npcSkills[4].name}: </span>{npcSkills[4].desc}</li>
+                <li><span>{npcSkills[5].name}: </span>{npcSkills[5].desc}</li>
+            </>
+            
+        )
+    }
+    
+    
+
+
+
+    
+}
+
+function actions(){
+    if(npcActions.length === 1){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+            </>
+        )
+    }if(npcActions.length === 2){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+                <li><span>{npcActions[1].name}: </span>{npcActions[1].desc}</li>
+
+            </>
+        )
+    }if(npcActions.length === 3){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+                <li><span>{npcActions[1].name}: </span>{npcActions[1].desc}</li>
+                <li><span>{npcActions[2].name}: </span>{npcActions[2].desc}</li>
+
+
+            </>
+        )
+    }if(npcActions.length === 4){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+                <li><span>{npcActions[1].name}: </span>{npcActions[1].desc}</li>
+                <li><span>{npcActions[2].name}: </span>{npcActions[2].desc}</li>
+                <li><span>{npcActions[3].name}: </span>{npcActions[3].desc}</li>
+
+
+
+            </>
+        )
+    }if(npcActions.length === 5){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+                <li><span>{npcActions[1].name}: </span>{npcActions[1].desc}</li>
+                <li><span>{npcActions[2].name}: </span>{npcActions[2].desc}</li>
+                <li><span>{npcActions[3].name}: </span>{npcActions[3].desc}</li>
+                <li><span>{npcActions[4].name}: </span>{npcActions[4].desc}</li>
+            </>
+        )
+    }if(npcActions.length === 6){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+                <li><span>{npcActions[1].name}: </span>{npcActions[1].desc}</li>
+                <li><span>{npcActions[2].name}: </span>{npcActions[2].desc}</li>
+                <li><span>{npcActions[3].name}: </span>{npcActions[3].desc}</li>
+                <li><span>{npcActions[4].name}: </span>{npcActions[4].desc}</li>
+                <li><span>{npcActions[5].name}: </span>{npcActions[5].desc}</li>
+
+            </>
+        )
+    }if(npcActions.length === 7){
+        return(
+            <>
+                <li><span>{npcActions[0].name}: </span>{npcActions[0].desc}</li>
+                <li><span>{npcActions[1].name}: </span>{npcActions[1].desc}</li>
+                <li><span>{npcActions[2].name}: </span>{npcActions[2].desc}</li>
+                <li><span>{npcActions[3].name}: </span>{npcActions[3].desc}</li>
+                <li><span>{npcActions[4].name}: </span>{npcActions[4].desc}</li>
+                <li><span>{npcActions[5].name}: </span>{npcActions[5].desc}</li>
+                <li><span>{npcActions[6].name}: </span>{npcActions[6].desc}</li>
+            </>
+        )
+    }
+    
+    
+    
 }
